040b9008481de33926b6132f25f593b0d3c97613
tobiasflores <tobiymin@gmail.com>
2026-09-25 19:11:12 -0300

    arreglo movimiento de la calabaza para que pueda desplazarse por todo el ancho de la pantalla

--- scripts/caramelos/calabaza.gd
+++ scripts/caramelos/calabaza.gd
@@ -3,9 +3,15 @@ extends Area2D
 signal atrapo_objeto(objeto: Node2D)
 
 @export var velocidad: float = 500.0
+@export var margen: float = 50.0
 
-var limite_izquierdo: float = 50.0
-var limite_derecho: float = 1100.0
+var limite_izquierdo: float
+var limite_derecho: float
+
+func _ready() -> void:
+	var ancho_pantalla: float = get_viewport_rect().size.x
+	limite_izquierdo = margen
+	limite_derecho = ancho_pantalla - margen
 
 func _process(delta: float) -> void:
 	var direccion: float = 0.0
@@ -14,8 +20,8 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		direccion += 1.0
 
-	position.x += direccion * velocidad * delta
-	position.x = clamp(position.x, limite_izquierdo, limite_derecho)
+	global_position.x += direccion * velocidad * delta
+	global_position.x = clamp(global_position.x, limite_izquierdo, limite_derecho)
 
 func _on_area_entered(area: Area2D) -> void:
 	atrapo_objeto.emit(area)
